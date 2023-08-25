2a4a17880d69b6fbcc948578292b6ccd1b45ae28
jayHuggie <jhl011@ucsd.edu>
2023-08-25 09:41:16 +0900

    updated information

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ export default function Footer() {
         py={4}
         align="center"
       >
-        <Text>© 2023 Eldora Boo.</Text>
+        <Text>© 2023 Eldora Boo</Text>
       </Container>
     </Box>
   );
