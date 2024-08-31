273bc70d89df007eb8692f3385e4e2d6dc43d8cd
Jae Hyuk (Joseph) Lee <jhl011@ucsd.edu>
2024-09-01 04:20:47 +0900

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -53,7 +53,6 @@ export default function Contact({ color }) {
                 <FaLinkedin onClick={linkedin} size={28} />
                 <FaGithub onClick={github} size={28} />
                 <FaEnvelope onClick={email} size={28} />
-                <FaLinkedin onClick={linkedin} size={28} />
               </HStack>
             </Center>
           </Stack>
